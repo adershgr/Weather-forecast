ee2dee3d82c48741656015d8c8b682b508f278fc
adershgr <mail4me.adersh@gmail.com>
2022-08-08 19:52:46 +0530

    main files in src are uploaded

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
+import TimeAndLocation from './components/TimeAndLocation';
+import TemperatureAndDetails from './components/TemperatureAndDetails';
+import Forecast from './components/Forecast';
+
+function App() {
+  return (
+    <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400">
+      <TopButtons />
+      <Inputs />
+
+      <TimeAndLocation />
+      <TemperatureAndDetails />
+      <Forecast title ="hourly forecast"/>
+      <Forecast title ="daily forecast"/>
+
+    </div>
+  );
+}
+
+export default App;
